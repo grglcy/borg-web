bd31338b9ec60fcdd01d0afcb442fd784b4e4d18
George Lacey <git@george.ooo>
2021-05-06 14:18:48 +0100

    Display basic borg repo information

--- borgweb/borg/views.py
+++ borgweb/borg/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from .models import Repo, Label
 
 
 def index(request):
-    return HttpResponse("Index here")
+    repo_list = Repo.objects.all()
+    label_list = Label.objects.all()
+    # template = loader.get_template('borg/index.html')
+    context = {
+        'repo_list': repo_list,
+        'label_list': label_list,
+        }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'borg/index.html', context)
