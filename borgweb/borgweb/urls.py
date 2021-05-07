373434c3ef7c62ac3962861be823ff1d070a164f
George Lacey <git@george.ooo>
2021-05-07 16:08:11 +0100

    Access borg app from root url

--- borgweb/borgweb/urls.py
+++ borgweb/borgweb/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    path('borg/', include('borg.urls')),
+    path('', include('borg.urls')),
     path('admin/', admin.site.urls),
 ]
