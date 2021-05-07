9f71901a4975647286f303a5311d9b8c867e7aa3
George Lacey <git@george.ooo>
2021-05-07 20:31:49 +0100

    Move label to start

--- borgweb/borg/forms/repoform.py
+++ borgweb/borg/forms/repoform.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class RepoForm(forms.Form):
+    label = forms.CharField(label='Label')
     fingerprint = forms.CharField(label='Fingerprint')
     location = forms.CharField(label='Location')
     last_modified = forms.DateTimeField(label='Last Modified', input_formats=["%Y-%m-%dT%H:%M:%S.%z"])
-    label = forms.CharField(label='Label')
