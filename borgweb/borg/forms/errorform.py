80489ffd841b8fe6acbb520aa952c64b809c980b
George Lacey <git@george.ooo>
2021-05-08 10:51:22 +0100

    Correct error and time labels

--- borgweb/borg/forms/errorform.py
+++ borgweb/borg/forms/errorform.py
@@ -3,5 +3,5 @@ from django import forms
 
 class ErrorForm(forms.Form):
     label = forms.CharField(label='Label')
-    error = forms.CharField(label='Fingerprint')
-    time = forms.DateTimeField(label='Last Modified', input_formats=["%Y-%m-%dT%H:%M:%S.%z"])
+    error = forms.CharField(label='Error')
+    time = forms.DateTimeField(label='Time', input_formats=["%Y-%m-%dT%H:%M:%S.%z"])
