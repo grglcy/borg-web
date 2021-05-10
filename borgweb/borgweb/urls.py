ecc94ec1740adefe0de1860649d5f11fc05f8655
George Lacey <git@george.ooo>
2021-05-10 02:08:57 +0100

    Move account urls to root

--- borgweb/borgweb/urls.py
+++ borgweb/borgweb/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('borg.urls')),
 ]
