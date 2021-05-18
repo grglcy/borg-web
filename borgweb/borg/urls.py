0ad734eb728eca20bedf4dbebfe332a5fa7465bd
George Lacey <git@george.ooo>
2021-05-18 17:47:46 +0100

    Cache entire index for 10 seconds

--- borgweb/borg/urls.py
+++ borgweb/borg/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', cache_page(10)(views.index), name='index'),
     path('post/repo', views.post_repo, name='repo'),
     path('post/archive', views.post_archive, name='archive'),
     path('post/error', views.post_error, name='error'),
