6e1dbb2bb473b6244ca35635f5ec7e4073e1893b
George Lacey <git@george.ooo>
2021-07-11 10:16:44 +0100

    Create repo.js stub

---
+++ borgweb/borg/static/borg/js/repo.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded", function () {
+    // $.getJSON( "repo_daily.json", function( json ) {
+    //     draw_time_graph("daily_backup_size", json.repos, json.dates, json.units);
+    //  });
+}, false);
