a4cae2ee2c5202f449b0351f2df7a221d8f8b4d5
George Lacey <git@george.ooo>
2022-04-06 13:34:41 +0100

    Re-enable graphs

--- borgweb/borg/static/borg/js/index.js
+++ borgweb/borg/static/borg/js/index.js
@@ -32,10 +32,10 @@ window.addEventListener("DOMContentLoaded", function () {
     })
 
 
-    // $.getJSON("repo_daily.json", function (json) {
-    //     draw_time_graph("daily_backup_size", json.repos, json.dates, json.units);
-    // });
-    // $.getJSON("repo_monthly.json", function (json) {
-    //     draw_time_graph("monthly_backup_size", json.repos, json.dates, json.units);
-    // });
+    $.getJSON("repo_daily.json", function (json) {
+        draw_time_graph("daily_backup_size", json.repos, json.dates, json.units);
+    });
+    $.getJSON("repo_monthly.json", function (json) {
+        draw_time_graph("monthly_backup_size", json.repos, json.dates, json.units);
+    });
 }, false);
