f09a64c26e904f5af06537fa74d166eb09821ae2
George Lacey <git@george.ooo>
2022-04-11 08:08:34 +0100

    Correct precedence

--- borgweb/borg/urls.py
+++ borgweb/borg/urls.py
@@ -4,9 +4,8 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-    # Pages
+    # Index Page
     path('', cache_page(60)(views.index), name='index'),
-    path('repo/<str:repo_label>', cache_page(60)(views.repo), name='repo'),
 
     path('repo-list.json', cache_page(60)(views.repo_list_json), name='repo list'),
 
@@ -18,6 +17,9 @@ urlpatterns = [
     path('repo/<str:repo_label>/size.json', cache_page(60)(views.repo_size_json), name='repo size json'),
     path('repo/<str:repo_label>/recent-errors.json', cache_page(60)(views.repo_recent_errors_json),
          name='repo recent errors json'),
+
+    # Repo page
+    path('repo/<str:repo_label>', cache_page(60)(views.repo), name='repo'),
 
     # POST
     path('post/repo', views.post_repo, name='post repo'),
