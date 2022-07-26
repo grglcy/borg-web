c506d9fcfaa5e5a7bb469fa0a6905cfe7e209d3c
George Lacey <git@george.ooo>
2022-07-26 15:25:27 +0100

    Add field to allow repo hiding

--- borgweb/borg/models/label.py
+++ borgweb/borg/models/label.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Label(models.Model):
     label = models.TextField(blank=True, unique=True)
+    visible = models.BooleanField(default=True)
 
     def __str__(self):
         return self.label
