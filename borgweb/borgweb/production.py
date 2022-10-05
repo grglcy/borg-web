53568e360f38072006d43f2b9dde2150db4ead03
George Lacey <git@george.ooo>
2022-10-05 16:28:46 +0100

    Add logging

--- borgweb/borgweb/production.py
+++ borgweb/borgweb/production.py
@@ -133,6 +133,37 @@ CACHES = {
     }
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "root": {"level": "INFO", "handlers": ["file"]},
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django/borgweb.log",
+            "formatter": "app",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True
+        },
+    },
+    "formatters": {
+        "app": {
+            "format": (
+                u"%(asctime)s [%(levelname)-8s] "
+                "(%(module)s.%(funcName)s) %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+    },
+}
+
+
 # security
 SESSION_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
